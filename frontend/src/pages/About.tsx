000425252d383fe4b0e431ca3a4b2096ba0242a8
Adil Zamal <adil.zamal@juspay.in>
2024-03-17 02:03:17 +0530

    About using tailwind css

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -12,63 +12,54 @@ const About: React.FC = () => {
     <div>
       <Navbar />
       <PageHeader heading="About Us" />
-      {/* About Start */}
-      <div className="container-fluid py-5">
-        <div className="container">
-          <div className="row align-items-center">
-            <div className="col-lg-5">
-              <img
-                className="img-fluid rounded mb-5 mb-lg-0"
-                src="img/about-1.jpg"
-                alt=""
-              />
+      <div className="container py-20 max-w-7xl grid grid-cols-1 sm:grid-cols-3 items-center md:gap-7">
+        <div className="">
+          <img
+            className="rounded"
+            src="img/about-1.jpg"
+            alt=""
+          />
+        </div>
+        <div className="col-span-2">
+          <p className="">
+            <span className="pr-2">Learn About Us</span>
+          </p>
+          <h1 className="mb-4 text-cyan-600 font-moto text-3xl">Best School For Your Kids</h1>
+          <p>
+            At Al Sufiaan School, we are dedicated to providing a nurturing
+            and stimulating environment where your child can thrive
+            academically, socially, and emotionally. Here are some reasons
+            why we believe we are the best choice for your children
+          </p>
+          <div className="flex pt-3 pb-6">
+            <div className="basis-1/2 md:basis-1/4 pr-4">
+              <img className="rounded" src="img/about-2.jpg" alt="" />
             </div>
-            <div className="col-lg-7">
-              <p className="section-title pr-5">
-                <span className="pr-2">Learn About Us</span>
-              </p>
-              <h1 className="mb-4 text-primary">Best School For Your Kids</h1>
-              <p>
-                At Al Sufiaan School, we are dedicated to providing a nurturing
-                and stimulating environment where your child can thrive
-                academically, socially, and emotionally. Here are some reasons
-                why we believe we are the best choice for your children
-              </p>
-              <div className="row pt-2 pb-4">
-                <div className="col-6 col-md-4">
-                  <img className="img-fluid rounded" src="img/about-2.jpg" alt="" />
-                </div>
-                <div className="col-6 col-md-8">
-                  <ul className="list-inline m-0">
-                    <li className="py-2 border-top border-bottom">
-                      <i className="fa fa-check text-primary mr-3"></i>Academic
-                      Excellence & Engaging Curriculum
-                    </li>
-                    <li className="py-2 border-bottom">
-                      <i className="fa fa-check text-primary mr-3"></i>Holistic
-                      Development & Modern Facilities
-                    </li>
-                    <li className="py-2 border-bottom">
-                      <i className="fa fa-check text-primary mr-3"></i>Safe and
-                      Supportive Environment
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <a href="" className="btn btn-primary mt-2 py-2 px-4">
-                Learn More
-              </a>
+            <div className="basis-1/2 md:basis-3/4">
+              <ul className="list-inline m-0">
+                <li className="py-2 border-b">
+                  <i className="fa fa-check text-cyan-600 mr-3"></i>Academic
+                  Excellence & Engaging Curriculum
+                </li>
+                <li className="py-2 border-b">
+                  <i className="fa fa-check text-cyan-600 mr-3"></i>Holistic
+                  Development & Modern Facilities
+                </li>
+                <li className="py-2 border-b">
+                  <i className="fa fa-check text-cyan-600 mr-3"></i>Safe and
+                  Supportive Environment
+                </li>
+              </ul>
             </div>
           </div>
+          <a href="/" className="bg-cyan-600 text-white py-3 px-4 rounded-3xl no-underline hover:no-underline text-sm font-semibold">
+            Learn More
+          </a>
         </div>
       </div>
-      {/* About End */}
-
-      {/* Facilities Start */}
       <div className="container grid md:grid-cols-3 gap-6 pt-5">
-            {facilities.map(facility => <FacilitiesCard title={facility.title} description={facility.description} iconName={facility.iconName} />)}
+        {facilities.map(facility => <FacilitiesCard title={facility.title} description={facility.description} iconName={facility.iconName} />)}
       </div>
-      {/* Facilities Start */}
       <div className="container pt-5 grid md:grid-cols-4">
         {teachers.map(teacher => <TeacherCard name={teacher.name} designation={teacher.designation} imageUrl={teacher.imageUrl} />)}
       </div>
